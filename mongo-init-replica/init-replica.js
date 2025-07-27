b5d7f3f54e8228d3065ed659a77e0dd02c8c96ef
Francesco Conforti <francesco.conforti03@icloud.com>
2025-07-27 16:11:48 +0200

    Update MongoDB replica config and healthcheck
    Changed MongoDB healthcheck in docker-compose.yml to use replica set status and increased timeout. Updated replica set member host in init-replica.js from 'mongo:27017' to 'localhost:27017' for improved local connectivity.

--- mongo-init-replica/init-replica.js
+++ mongo-init-replica/init-replica.js
@@ -1,8 +1,6 @@
-rs.initiate(
-  {
-    _id: "rs0",
-    members: [
-      { _id: 0, host: "mongo:27017" }
-    ]
-  }
-);
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "localhost:27017" }
+  ]
+});
